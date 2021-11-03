ad73a7a1314d5a96e71bd61d3293f05740c6f082
Yjan Gordon <yjangordon@YjansMBManitoba.lan>
2021-11-03 17:46:28 -0500

    added PanSTARRS cutout url production functionality

--- astro_tools.py
+++ astro_tools.py
@@ -7,6 +7,7 @@ from astroquery.image_cutouts.first import First
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 from astropy.io import fits
+from astropy.table import Table
 
 ####main functionality
 
@@ -156,6 +157,67 @@ class stats:
         
         return fracpluserrs
 
+
+class PanSTARRS:
+    ###functions for getting panstarrs data
+    ###get images obtained from https://ps1images.stsci.edu/ps1image.html
+
+    def getimages(ra,dec,size=240,filters="grizy"):
+    
+        """Query ps1filenames.py service to get a list of images
+        
+            ra, dec = position in degrees
+            size = image size in pixels (0.25 arcsec/pixel)
+            filters = string with filters to include
+            Returns a table with the results
+            """
+    
+        service = "https://ps1images.stsci.edu/cgi-bin/ps1filenames.py"
+        url = ("{service}?ra={ra}&dec={dec}&size={size}&format=fits"
+               "&filters={filters}").format(**locals())
+        table = Table.read(url, format='ascii')
+        return table
+
+
+    def geturl(ra, dec, size=240, output_size=None, filters="grizy", format="jpg", color=False):
+    
+        """Get URL for images in the table
+        
+            ra, dec = position in degrees
+            size = extracted image size in pixels (0.25 arcsec/pixel)
+            output_size = output (display) image size in pixels (default = size).
+            output_size has no effect for fits format images.
+            filters = string with filters to include
+            format = data format (options are "jpg", "png" or "fits")
+            color = if True, creates a color image (only for jpg or png format).
+            Default is return a list of URLs for single-filter grayscale images.
+            Returns a string with the URL
+            """
+    
+        if color and format == "fits":
+            raise ValueError("color images are available only for jpg or png formats")
+        if format not in ("jpg","png","fits"):
+            raise ValueError("format must be one of jpg, png, fits")
+        table = PanSTARRS.getimages(ra,dec,size=size,filters=filters)
+        url = ("https://ps1images.stsci.edu/cgi-bin/fitscut.cgi?"
+               "ra={ra}&dec={dec}&size={size}&format={format}").format(**locals())
+        if output_size:
+            url = url + "&output_size={}".format(output_size)
+        # sort filters from red to blue
+        flist = ["yzirg".find(x) for x in table['filter']]
+        table = table[np.argsort(flist)]
+        if color:
+            if len(table) > 3:
+                # pick 3 filters
+                table = table[[0,len(table)//2,len(table)-1]]
+            for i, param in enumerate(["red","green","blue"]):
+                url = url + "&{}={}".format(param,table['filename'][i])
+        else:
+            urlbase = url + "&red="
+            url = []
+            for filename in table['filename']:
+                url.append(urlbase+filename)
+        return url
 
 
 
